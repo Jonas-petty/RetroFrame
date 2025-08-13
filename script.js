b28f836b418d1a9c4de148487dc33c4920555737
Jonas Felix <jonaspetty@hotmail.com>
2025-08-13 17:57:32 +0000

    feat: create grid based on size informed

---
+++ script.js
@@ -0,0 +1,27 @@
+let gridSize = +document.querySelector("#frame-size").value;
+const frameContainer = document.querySelector("#frame-container");
+
+function createCell(id, className, width, height) {
+    const cell = document.createElement("div");
+    cell.id = id;
+    cell.className = className;
+    cell.style["width"] = `${width}%`;
+    cell.style["height"] = `${height}%`;
+
+    return cell;
+}
+
+function createGrid(frame, gridSize) {
+    if (gridSize <= 100) {
+        const numberOfCells = gridSize * gridSize;
+        for (i = 0; i < numberOfCells; i++) {
+            const cellSize = 100 / gridSize;
+            const cell = createCell(i, "cell", cellSize, cellSize);
+            frame.appendChild(cell);
+        }
+    } else {
+        alert("The maximum size is 100, please try again!");
+    }
+}
+
+createGrid(frameContainer, gridSize);
